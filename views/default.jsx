915c2e7cdeb338431987742b04c066c0388aa409
Chris Wehner <chrisw7128@gmail.com>
2023-04-22 12:32:04 -0700

    more stuff

--- views/default.jsx
+++ views/default.jsx
@@ -1,16 +1,36 @@
 const React = require('react')
 
 function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
+  return (
+      <html>
+<head>
+        <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        />
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+          <body>
+          <nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/places">Places</a>
+    </li>
+    <li>
+      <a href="/places/new">Add Place</a>
+    </li>
+  </ul>
+</nav>
+
+              {html.children}
+          </body>
+      </html>
+  )
 }
+
 
 module.exports = Def
