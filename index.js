1e896ca0a12cbf7ade59f1679671bd95a8ef87bd
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-04-25 16:45:41 -0700

    Fixing errors. app was not defined

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Modules and Globals
 require("dotenv").config();
 const express = require("express");
+const app = express()
 const methodOverride = require("method-override");
 
 // Express Settings
@@ -10,6 +11,10 @@ app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
+
 
 app.get("/", (req, res) => {
   res.render("home");
