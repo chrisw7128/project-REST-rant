26f96a5dacf5e800ec5fe769bf0a4ae2e257b8d2
Chris Wehner <chrisw7128@gmail.com>
2023-04-27 14:52:52 -0700

    everything says ADD URL

--- models/places.js
+++ models/places.js
@@ -1,16 +1,12 @@
-module.exports = [
-  {
-    name: "Shang",
-    city: "Las Vegas",
-    state: "NV",
-    cuisines: "Chinese Noodles",
-    pic: "/images/shang.jpg",
-  },
-  {
-    name: "Flower Child",
-    city: "Las Vegas",
-    state: "NV",
-    cuisines: "American casual",
-    pic: "/images/flower_child.jpg",
-  },
-];
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: "Anytown" },
+  state: { type: String, default: "USA" },
+  founded: Number,
+});
+
+module.exports = mongoose.model("Place", placeSchema);
