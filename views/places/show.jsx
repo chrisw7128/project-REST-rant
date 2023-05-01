fa0265f96a54c4803604604757ee3dc3265dc169
Chris Wehner <chrisw7128@gmail.com>
2023-05-01 13:05:02 -0700

    activity 20 done except for shows.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,27 +3,32 @@ const Def = require('../default')
 
 function show(data) {
   return (
-    <Def>
-      <main>
-        <h1>{data.place.name}</h1>
-        <section>currently unrated</section>
-        <section>Comments</section>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
+<Def>
+  <main>
+    <div className="row">
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+        ...
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        ...
+    </div>
+  </main>
+</Def>
 
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
-
-
-
-
-      </main>
-    </Def>
   )
-}
+};
 
 module.exports = show
