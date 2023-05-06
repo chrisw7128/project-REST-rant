32705106c6a8a14bf54fe0c1ee92284f3d410007
Chris Wehner <chrisw7128@gmail.com>
2023-05-06 08:50:49 -0700

    finished part 11

--- controllers/places.js
+++ controllers/places.js
@@ -46,7 +46,14 @@ router.get("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  res.send("PUT /places/:id stub");
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch(err => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 router.delete("/:id", (req, res) => {
@@ -54,7 +61,13 @@ router.delete("/:id", (req, res) => {
 });
 
 router.get("/:id/edit", (req, res) => {
-  res.send("GET edit form stub");
+  db.Place.findById(req.params.id)
+    .then(place => {
+      res.render("places/edit", { place });
+    })
+    .catch(err => {
+      res.render("error404");
+    });
 });
 
 router.post("/:id/comment", (req, res) => {
@@ -85,8 +98,15 @@ router.post("/", (req, res) => {
     });
 });
 
-router.delete("/:id/rant/:rantId", (req, res) => {
-  res.send("GET /places/:id/rant/:rantId stub");
+router.delete("/:id", (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(place => {
+      res.redirect("/places");
+    })
+    .catch(err => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 module.exports = router;
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -8,37 +8,43 @@ function edit_form (data) {
             <h1>Edit Place</h1>
           </main>
           <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-  <div className="form-group">
-      <label htmlFor="name">Place Name</label>
-      <input 
-        className="form-control" 
-        id="name" 
-        name="name" 
-        value={data.place.name} 
-      required />
-  </div>
-          <div className="form-group">
-            <label htmlFor="pic">Place Picture</label>
-            <input type="url" className="form-control" id="pic" name="pic" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="city">City</label>
-            <input type="url" className="form-control" id="city" name="city" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="state">State</label>
-            <input type="url" className="form-control" id="state" name="state" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="cuisines">Cuisines</label>
-            <input type="url" className="form-control" id="cuisines" name="cuisines" required />
-          </div>
-          <input className="btn btn-primary" type="submit" value="Add Place" />
+                <div className="form-group">
+                    <label htmlFor="name">Place Name</label>
+                    <input 
+                      className="form-control" 
+                      id="name" 
+                      name="name" 
+                      value={data.place.name} 
+                    required />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="pic">Place Picture</label>
+                  <input type="url" className="form-control" id="pic" name="pic" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="city">City</label>
+                  <input type="url" className="form-control" id="city" name="city" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="state">State</label>
+                  <input type="url" className="form-control" id="state" name="state" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="cuisines">Cuisines</label>
+                  <input type="url" className="form-control" id="cuisines" name="cuisines" required />
+                </div>
+                <div className="form-group col-sm-4">
+                <label htmlFor="founded">Founded</label>
+                <input className="form-control" 
+                  id="founded" 
+                  name="founded" 
+                  value={data.place.founded} 
+                />
+                </div>
+            <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
         </Def>
     )
 }
 
 module.exports = edit_form
-
-
